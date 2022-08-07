445c4be057d0e4736906a00956eacaa2e85ba6d4
Mark Politiko <politikomark@yandex.ru>
2022-08-07 07:34:24 +0300

    Пятый проект для второй итерации проверки (изменен нейминг, устранено дублирование кода, один класс попапов, required, поправлена верстка карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,21 @@
-const profileOpenButton = document.querySelector('.profile__edit-button');
-const profileButtonClose = document.querySelector('.edit__close-button');
-const profileForm = document.querySelector('.edit__form');
-const profileButtonEdit = document.querySelector('.edit');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileForm = document.querySelector('.popup_edit-profile');
+const popupProfileEditWindow = document.querySelector('.popup_open_edit-window');
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
-const profileInputName = document.querySelector('.edit__input_type_name');
-const profileInputDescription = document.querySelector('.edit__input_type_description');
+const profileInputName = document.querySelector('.popup__input_type_name');
+const profileInputDescription = document.querySelector('.popup__input_type_description');
 
 const profileAddButton = document.querySelector('.profile__add-button');
-const addButtonClose = document.querySelector('.add__close-button');
-const addForm = document.querySelector('.add__form');
-const toAdd = document.querySelector('.add');
-const addInputName = document.querySelector('.add__input_type_name'); // название фото
-const addInputLink = document.querySelector('.add__input_type_link'); // ссылка на фото
+const popupAddForm = document.querySelector('.popup_form_add-element');
+const popupAddElement = document.querySelector('.popup_open_add-window');
+const popupAddElementNameInput = document.querySelector('.popup__input_type_place'); // название фото
+const popupAddElementLinkInput = document.querySelector('.popup__input_type_link'); // ссылка на фото
 
 const elementsTemplate = document.querySelector('#elements-template').content;
 const elements = document.querySelector('.elements');
-const elementsImage = document.querySelector('.elements__image');
-const elementsTitle = document.querySelector('.elements__title');
 
-const popupButtonClose = document.querySelectorAll('.popup__close-button');
+const popupButtonsClose = document.querySelectorAll('.popup__close-button');
 const popupImage = document.querySelector('.popup__image');
 const popupOpenImage = document.querySelector('.popup_image_open');
 const popupCaption = document.querySelector('.popup__caption');
@@ -27,24 +23,21 @@ const popupCaption = document.querySelector('.popup__caption');
 
 // механизм редактирования профиля
 
-function editShow() { // форма для изменения профиля
+function showPopupProfile() { // форма для изменения профиля
   profileInputName.value = profileName.textContent;
   profileInputDescription.value = profileDescription.textContent;
-  profileButtonEdit.classList.add('edit_show');
+  popupProfileEditWindow.classList.add('popup_show');
 }
 
-function formSaveChanges(evt) { // сохранение изменений профиля
+function submitFormChanges(evt) { // сохранение изменений профиля
   evt.preventDefault();
+  const evtTarget = evt.target.closest('.popup');
   profileName.textContent = profileInputName.value;
   profileDescription.textContent = profileInputDescription.value;
-  editClose();
+  closePopup(evtTarget);
 }
 
-function editClose() { // закрытие формы редактирования профиля
-  profileButtonEdit.classList.remove('edit_show');
-}
-
-function addElements() {
+function addInitialElements() {
   const initialCards = [
     {
       name: 'Архыз',
@@ -79,40 +72,28 @@ function addElements() {
   })
 }
 
-addElements();
-
-function addShow() {
-  toAdd.classList.add('add_show');
-}
-
-function addClose() { // закрытие формы добавления фото
-  toAdd.classList.remove('add_show');
-}
+addInitialElements();
 
 function createInitial(elementName, elementLink) {
   const newCard = elementsTemplate.querySelector('.elements__card').cloneNode(true);
-  const initialTitle = newCard.querySelector('.elements__title');
-  const initialImage = newCard.querySelector('.elements__image');
-  const elementsDeleteButton = newCard.querySelector('.elements__delete-button');
-  const likeButton = newCard.querySelectorAll('.elements__like-button');
+  const newElementTitle = newCard.querySelector('.elements__title');
+  const newElementImage = newCard.querySelector('.elements__image');
+  const likeButton = newCard.querySelector('.elements__like-button');
+  const newElementDeleteButton = newCard.querySelector('.elements__delete-button');
 
-  initialTitle.textContent = elementName;
-  initialImage.setAttribute('src', elementLink);
-  initialImage.setAttribute('alt', elementName);
+  newElementTitle.textContent = elementName;
+  newElementImage.setAttribute('src', elementLink);
+  newElementImage.setAttribute('alt', elementName);
 
-  likeButton.forEach(item => {
-    item.addEventListener('click', (evt) => {
-        evt.target.classList.toggle('elements__like-button_active');
-    })
+  likeButton.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('elements__like-button_active');
   })
 
-  elementsDeleteButton.addEventListener('click', (evt) => {
-    const evtTarget = evt.target;
-    const newCard = evtTarget.closest('.elements__card');
+  newElementDeleteButton.addEventListener('click', (evt) => {
     newCard.remove();
   })
 
-  initialImage.addEventListener('click', (evt) => {
+  newElementImage.addEventListener('click', (evt) => {
     const evtTarget = evt.target;
     popupImage.setAttribute('src', evtTarget.src);
     popupImage.setAttribute('alt', evtTarget.alt);
@@ -120,17 +101,17 @@ function createInitial(elementName, elementLink) {
     showPopup(popupOpenImage);
   })
 
- return newCard;
+  return newCard;
 
 }
 
-function submitAddElement (evt) {
+function submitAddElementForm (evt) {
   evt.preventDefault();
-  const evtTarget = evt.target.closest('.add');
-  if (addInputName.value && addInputLink.value) {
-  elements.prepend(createInitial (addInputName.value, addInputLink.value))
+  const evtTarget = evt.target.closest('.popup');
+  if (popupAddElementNameInput.value && popupAddElementLinkInput.value) {
+  elements.prepend(createInitial (popupAddElementNameInput.value, popupAddElementLinkInput.value))
   }
-  addClose(evtTarget);
+  closePopup(evtTarget);
   evt.target.reset();
 }
 
@@ -142,8 +123,8 @@ function closePopup(popupClose) {
   popupClose.classList.remove('popup_show');
 }
 
-function toClosePopupButton() {
-  popupButtonClose.forEach((item) => {
+function initializeButtonsListeners() {
+  popupButtonsClose.forEach((item) => {
     item.addEventListener('click', (evt) => {
         const evtTarget = evt.target.closest('.popup');
         closePopup(evtTarget);
@@ -151,13 +132,13 @@ function toClosePopupButton() {
   })
 }
 
-toClosePopupButton();
+initializeButtonsListeners();
 
-profileOpenButton.addEventListener('click', editShow);
-profileForm.addEventListener('submit', formSaveChanges);
-profileButtonClose.addEventListener('click', editClose);
+profileEditButton.addEventListener('click', showPopupProfile);
+profileForm.addEventListener('submit', submitFormChanges);
 
-profileAddButton.addEventListener('click', addShow);
-addButtonClose.addEventListener('click', addClose);
+profileAddButton.addEventListener('click', () => {
+  showPopup(popupAddElement)
+});
 
-addForm.addEventListener('submit', submitAddElement);
+popupAddForm.addEventListener('submit', submitAddElementForm);
